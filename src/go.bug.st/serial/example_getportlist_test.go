fcbdb74fa06cb5c8ca784c54ecc96fc919f8a914
Daniel Renne <Daniel.Renne@atlona.com>
2022-03-08 14:18:33 -0500

    Removed Git directory from go.bug.st/serial

---
+++ src/go.bug.st/serial/example_getportlist_test.go
@@ -0,0 +1,28 @@
+//
+// Copyright 2014-2021 Cristian Maglie. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+package serial_test
+
+import (
+	"fmt"
+	"log"
+
+	"go.bug.st/serial"
+)
+
+func ExampleGetPortsList() {
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		fmt.Println("No serial ports found!")
+	} else {
+		for _, port := range ports {
+			fmt.Printf("Found port: %v\n", port)
+		}
+	}
+}
---
+++ src/go.bug.st/serial/example_serialport_test.go
@@ -0,0 +1,31 @@
+//
+// Copyright 2014-2021 Cristian Maglie. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+package serial_test
+
+import (
+	"fmt"
+	"log"
+
+	"go.bug.st/serial"
+)
+
+func ExampleSerialPort_SetMode() {
+	port, err := serial.Open("/dev/ttyACM0", &serial.Mode{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	mode := &serial.Mode{
+		BaudRate: 9600,
+		Parity:   serial.NoParity,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+	}
+	if err := port.SetMode(mode); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Port set to 9600 N81")
+}
